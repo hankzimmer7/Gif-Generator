e2d3002b94a860e982c488286607f5ac7635a215
hankzimmer7 <hankzimmer7@gmail.com>
2018-07-06 18:38:10 -0400

    adds button to see more gifs and adds titles to gifs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Gif-Generator
+
+## Description
+
+This website searches for gifs based on which button the user clicks. The text on the button will determine which gifs are displayed. The user may add a new button by typing text into the input box and clicking the submit button.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,17 @@
    //------------Declare global variables-------------------------------
    var buttonArray = ["winter", "dogs", "horses", "rain", "anime", "math", "beer", "atlanta", "okonomiyaki"];
+   var currentGifSearch;
+   var imageArray = [];
 
    //------------Define functions--------------------------------------
 
    //Function to generate each of the buttons that may be clicked to display gifs
    function displayGifButtons() {
 
+       //First empty the buttons area
        $("#gif-buttons").empty();
 
+       //For each element in the array, display a button
        buttonArray.forEach(element => {
            var newButton = $("<button>");
            newButton.addClass("gif-button btn btn-light my-2 mx-1");
@@ -19,38 +23,79 @@
    //Function to query to giphy api and generate new gif images
    function getGifs(gifName) {
 
-       var q = gifName
+       var q = gifName;
        var apiKey = "dc6zaTOxFJmzC";
-       var limit = 10;
-       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + apiKey + "&limit=" + limit;
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + apiKey;
 
-       console.log(queryURL);
        $.ajax({
            url: queryURL,
            method: "GET"
        }).then(function (response) {
 
-           console.log(response);
-           var imageArray = response.data;
+           //Place the response data into the global array
+           imageArray = response.data;
 
            console.log(imageArray);
 
-           imageArray.forEach(function (element, index) {
-               console.log(element.images.original.url);
-               console.log(element.images.fixed_width.url);
-               var imageUrl = element.images.downsized_medium.url;
-               var image = $("<img>");
-               image.attr("src", imageUrl);
-               //   image.attr("class", "img-fluid d-block mx-auto mt-4");
-                 image.attr("class", "img-fluid m-1");
-               //    image.attr("class", "gif-image");
-               image.attr("alt", gifName + " gif");
-               $("#gif-display-area").append(image);
-           });
+           //Display the first 10 gifs
+           for (var i = 0; i < 10; i++) {
+               displayGif(i);
+           }
+
+           //Display the See More Button
+           displaySeeMoreButton();
        });
    };
 
-   //------------Start the scripts once the document is ready--------------
+   //Function that appends one gif to the page
+   function displayGif(index) {
+
+       var element = imageArray[index];
+
+       //Place the image attributes in variables
+       var imageUrlAnimated = element.images.original.url;
+       var imageUrlStill = element.images.original_still.url;
+       var imageRating = element.rating;
+
+       //Create a figure tag for the image and the caption
+       var figure = $("<figure>");
+       figure.addClass("figure");
+
+       //Create an image tag
+       var image = $("<img>");
+       image.attr("src", imageUrlStill);
+       image.addClass("figure-img img-fluid rounded m-1");
+       image.attr("alt", currentGifSearch + " gif");
+       image.attr("url-animated", imageUrlAnimated);
+       image.attr("url-still", imageUrlStill);
+       image.attr("animation-state", "still");
+
+       //Create a caption with the title and rating
+       var title = $("<figcaption>");
+       title.addClass("figure-caption");
+       title.text("Title: " + element.title.toUpperCase());
+       var rating = $("<figcaption>");
+       rating.addClass("figure-caption");
+       rating.text("Rating: " + imageRating.toUpperCase());
+
+       //Append the image and caption to the figure
+       figure.append(image, title, rating);
+
+       //Append the figure to the page
+       $("#gif-display-area").append(figure);
+   }
+
+   function displaySeeMoreButton() {
+       //Create a "See More" button
+       var seeMoreButton = $("<button>");
+       seeMoreButton.addClass("btn btn-primary btn-block see-more-button");
+       seeMoreButton.text("See More");
+
+       //Append the "Add More Gifs" button to the page
+       $("#see-more-button").append(seeMoreButton);
+   }
+
+   //------------Start these scripts once the document is ready--------------
    $(document).ready(function () {
 
        //Initially display the Gif buttons
@@ -78,11 +123,41 @@
 
        })
 
+       //When the user clicks a gif button, clear the current gifs and display the desired gifs
        $(document).on("click", ".gif-button", function (event) {
+           //Save this as the current search term
+           currentGifSearch = $(this).text();
+
            //Clear the currently displayed gifs
            $("#gif-display-area").empty();
 
-           //Display the desired gifs
+           //Query the API for the desired term and display the first 10 gifs
            getGifs($(this).text());
+
+       });
+
+       //When the user clicks on a gif
+       $(document).on("click", ".figure-img", function (event) {
+
+           //Toggle whether the gif is animated or not
+           if ($(this).attr("animation-state") === "still") {
+               $(this).attr("src", $(this).attr("url-animated"));
+               $(this).attr("animation-state", "animated");
+           } else {
+               $(this).attr("src", $(this).attr("url-still"));
+               $(this).attr("animation-state", "still");
+           }
+       });
+
+       //When the user clicks on the "See More" button
+       $(document).on("click", ".see-more-button", function (event) {
+
+           //Display gifs 10 through 25
+           for (var i = 10; i < imageArray.length; i++) {
+               displayGif(i);
+           }
+
+           //Remove the See More Button
+           $("#see-more-button").empty();
        });
    });
